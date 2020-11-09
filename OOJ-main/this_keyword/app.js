bae4e7d58a5c98a630491e7c65591e1d48b73485
igiboi <36388765+igiboi@users.noreply.github.com>
2020-11-09 16:17:22 -0500

    this keyword

--- OOJ-main/this_keyword/app.js
+++ OOJ-main/this_keyword/app.js
@@ -9,5 +9,14 @@ calcAge(1994);
 
 const calcAgeArrow = birthYear => {
   console.log(2037 - birthYear);
-  console.log(this); // returns parent function Global scale aka Window parent because of the arrow function
+  console.log(this); // returns parent function Global scale aka Window parent because of the arrow function does not get its own this keyword
 }
+
+const jonas = {
+  year: 1991,
+  calcAge: function() {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
